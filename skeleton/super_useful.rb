caec61e8bca882d12b01f314650ec22098051cf8
vitor <vitor.grosskopf@gmail.com>
2023-10-21 11:38:39 -0300

    Finished project?

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,4 +1,17 @@
 # PHASE 2
+class CoffeeError < StandardError
+  def message
+    "I can't have any more caffeine. "
+  end
+end
+
+class NotAFruitError < StandardError
+  def message
+    "It is not a Fruit. You tricked me. * runs away *"
+  end
+end
+
+
 def convert_to_int(str)
   Integer(str)
 end
@@ -9,29 +22,43 @@ FRUITS = ["apple", "banana", "orange"]
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  elsif maybe_fruit.downcase == 'coffee'
+    raise CoffeeError
+  else
+    raise NotAFruitError
+  end
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue NotAFruitError => e
+    p e.message
+  rescue CoffeeError => e
+    p e.message
+    p "Wanna try again?"
+    retry
+  end
+end
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError ("name cannot be blank") if name.empty?
+    raise ArgumentError ("Years known cannot be blank") if yrs_known.empty?
+    raise ArgumentError ("favorite past time cannot be blank") if fav_pastime.empty?
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * convert_to_int(@yrs_known)}."
   end
 
   def do_friendstuff
@@ -39,7 +66,7 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
 
--- skeleton/user_script.rb
+++ skeleton/user_script.rb
@@ -1,10 +1,10 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
+# puts "'five' == #{convert_to_int('5')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('Vitor', '5', 'Draw')
 
 sam.talk_about_friendship
 sam.do_friendstuff
